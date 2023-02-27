5bd10a05b3be1d018fa3f5abaa42a2db68f4e5aa
Rodrigo <rss.999999999@gmail.com>
2023-02-26 23:05:13 -0300

    Alteração no código da aula 25

--- aula25.java
+++ aula25.java
@@ -1,13 +1,41 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class aula25 {
     public static void main(String[] args)  {
 
+        Locale.setDefault(Locale.US);               //imput double com "."". Ex: 4.5, por default está na vírgula. Ex.: 4,5
         Scanner sc = new Scanner(System.in);
 
-        String x;
-        x = sc.next();
+/*      String x;
+        x = sc.next(); //permite q eu armaze uma palavra e ela seja armazenada dentro do x
         System.out.println("Você digitou: "+x);   //sysout e tab
+ */
+
+/*      double y;
+        y = sc.nextDouble();
+        System.out.printf("Você digitou: %.4f%n", y);
+ */
+
+/*      char z;
+        z = sc.next().charAt(0);                //irá retornar apenas o primeiro caractere
+        System.out.println("Você digitou: " + z);
+ */
+
+        //para ler vários dados na mesma linha
+
+        String a;
+        int b;
+        double c;
+
+        a = sc.next();
+        b = sc.nextInt();
+        c = sc.nextDouble();
+        System.out.println("Dados digitados:");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
         
         sc.close();
 /* 
